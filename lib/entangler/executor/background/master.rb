39b09e4ab9b8eaedabc66fa5692fb414efca29bc
Dave Allie <dave@tanda.co>
2016-11-01 09:27:31 +1000

    Pass the ignore options to remote

--- lib/entangler/executor/background/master.rb
+++ lib/entangler/executor/background/master.rb
@@ -5,7 +5,8 @@ module Entangler
         protected
         def start_remote_slave
           require 'open3'
-          @remote_writer, @remote_reader, remote_err, @remote_thread = Open3.popen3("ssh -q #{@opts[:remote_user]}@#{@opts[:remote_host]} -p #{@opts[:remote_port]} -C \"source ~/.rvm/environments/default && entangler slave #{@opts[:remote_base_dir]}\"")
+          ignore_opts = @opts[:ignore].map{|regexp| "-i '#{regexp.inspect}'"}.join(' ')
+          @remote_writer, @remote_reader, remote_err, @remote_thread = Open3.popen3("ssh -q #{@opts[:remote_user]}@#{@opts[:remote_host]} -p #{@opts[:remote_port]} -C \"source ~/.rvm/environments/default && entangler slave #{@opts[:remote_base_dir]} #{ignore_opts}\"")
           remote_err.close
         end
 
